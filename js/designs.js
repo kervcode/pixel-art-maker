f546b423e0932a555b778f11878481a4dec94f73
Kervintz Noel <Kervintz84@hotmail.com>
2018-07-11 21:26:51 -0400

    Create canvas grid

--- js/designs.js
+++ js/designs.js
@@ -1,11 +1,47 @@
+let gridColor,
+gridHeight,
+gridWidth,
+button,
+tableGrid,
+tableRow,
+tableColumn;
+button = document.querySelector('form').lastElementChild;
+tableGrid = document.querySelector('#pixelCanvas');
+
+// let gridHeight = document.querySelector('#inputHeight').value;
+// let gridWidth = document.querySelector('#inputWidth').value;
 // Select color input
 
 // Select size input
 
 
 // When size is submitted by the user, call makeGrid()
+button.addEventListener("click", function(e){
+  gridHeight = document.querySelector('#inputHeight').value;
+  gridWidth = document.querySelector('#inputWidth').value;
+  e.preventDefault();
+  makeGrid();
+});
+
 
 function makeGrid() {
-  // Your code goes here!
-  
+  clearGrid();
+  // gridColor = document.querySelector('#colorPicker').value;
+  button = document.querySelector('form').lastElementChild;
+  //grid loop
+  for (i = 0; i < gridHeight; i++) {
+    tableRow = document.createElement('tr');
+    for (j = 0; j < gridWidth; j++) {
+      tableColumn = document.createElement('td');
+      tableRow.appendChild(tableColumn);
+    }
+    tableGrid.appendChild(tableRow);
+  }  
 }  
+
+// clear grid function
+function clearGrid(){ 
+  tableGrid.removeChild();
+}
+
+
